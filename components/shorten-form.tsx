39446119d28cc5e6ae9a9dd143555de6233a5274
Akash Sarker <akashsarker.dev@gmail.com>
2024-09-27 17:42:10 +0600

    database connect

--- components/shorten-form.tsx
+++ components/shorten-form.tsx
@@ -7,11 +7,12 @@ export default function ShortenForm() {
   const [url, setUrl] = useState('')
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
+    console.log(url)
   }
   return (
     <form onSubmit={handleSubmit} className='mb-4'>
       <div className='space-y-4'>
-        <Input type='url' required placeholder='Enter your url' />
+        <Input value={url} onChange={(e) => setUrl(e.target.value)} type='url' required placeholder='Enter your url' />
         <Button className='w-full p-2' type='submit'>Shorten</Button>
       </div>
     </form>
--- components/url-list.tsx
+++ components/url-list.tsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { CheckIcon, CopyIcon, EyeIcon } from "lucide-react";
 
 export default function UrlList() {
   return (
-    <div>UrlList</div>
-  )
+    <div>
+      <h2 className="text-2xl font-bold mb-2">Recent url</h2>
+      <ul className="space-y-2 ">
+        <li className="flex items-center justify-between gap-2 border-[1px] border-slate-200 rounded-md p-2">
+          <Link className="text-blue-500" target="_blank" href="https://www.google.com">https://www.google.com</Link>
+          <div className="flex items-center">
+            <Button
+              className="text-muted-foreground hover:bg-muted"
+              variant="ghost"
+              size="icon">
+              <CopyIcon className="w-4 h-4" />
+              <span className="sr-only">Copy</span>
+            </Button>
+            <span className="flex items-center">
+                <EyeIcon className="w-4 h-4" />
+                100 view
+            </span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 }
