a3c9ddbb62f8cd28ca3811bbb4eb3eb1c8fffbe4
Akash Sarker <akashsarker.dev@gmail.com>
2024-09-30 20:44:34 +0600

    api fetch

--- components/url-list.tsx
+++ components/url-list.tsx
@@ -1,15 +1,70 @@
-import Link from "next/link";
-import React from "react";
-import { Button } from "./ui/button";
-import { CheckIcon, CopyIcon, EyeIcon } from "lucide-react";
+// import Link from "next/link";
+// import React from "react";
+// import { Button } from "./ui/button";
+// import { CheckIcon, CopyIcon, EyeIcon } from "lucide-react";
+// import axios from "axios";
 
-export default function UrlList() {
+// export default async function  UrlList() {
+//   let data = await fetch('http://localhost:3000/api/urls')
+  
+//   let posts = await data.json()
+//   console.log(posts);
+  
+  
+//   return (
+//     <div>
+//       <h2 className="text-2xl font-bold mb-2">Recent url</h2>
+//       <ul className="space-y-2 ">
+//       {posts.map((post: any) => (
+//         <li key={post.id}>{post.title}</li>
+//       ))}
+//         {/* <li className="flex items-center justify-between gap-2 border-[1px] border-slate-200 rounded-md p-2">
+//           <Link className="text-blue-500" target="_blank" href="https://www.google.com">https://www.google.com</Link>
+//           <div className="flex items-center">
+//             <Button
+//               className="text-muted-foreground hover:bg-muted"
+//               variant="ghost"
+//               size="icon">
+//               <CopyIcon className="w-4 h-4" />
+//               <span className="sr-only">Copy</span>
+//             </Button>
+//             <span className="flex items-center">
+//                 <EyeIcon className="w-4 h-4" />
+//                 100 view
+//             </span>
+//           </div>
+//         </li> */}
+//       </ul>
+//     </div>
+//   );
+// }
+'use server'
+import { CopyIcon, EyeIcon } from 'lucide-react';
+import { revalidatePath } from 'next/cache'
+import { Button } from './ui/button';
+import Link from 'next/link';
+
+export default async function UrlList() {
+
+  const shortnerUrl = (code: string) => {
+    return `localhost:3000/${code}`
+  }
+
+
+
+const response = await fetch('http://localhost:3000/api/urls' ,{ cache: 'no-store' });
+const data = await response.json();
+revalidatePath('/',)
+  const urls = data.urls;
+  console.log(urls);
+
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-2">Recent url</h2>
-      <ul className="space-y-2 ">
-        <li className="flex items-center justify-between gap-2 border-[1px] border-slate-200 rounded-md p-2">
-          <Link className="text-blue-500" target="_blank" href="https://www.google.com">https://www.google.com</Link>
+    <ul>
+      {urls.map((url: any) => (
+      <li className="flex items-center justify-between gap-2 border-[1px] border-slate-200 rounded-md p-2">
+        <div>
+      <Link className="text-blue-500" target="_blank" href={shortnerUrl(url.code)}>{shortnerUrl(url.shortUrl)}</Link>
+        </div>
           <div className="flex items-center">
             <Button
               className="text-muted-foreground hover:bg-muted"
@@ -18,13 +73,13 @@ export default function UrlList() {
               <CopyIcon className="w-4 h-4" />
               <span className="sr-only">Copy</span>
             </Button>
-            <span className="flex items-center">
+            <span className="flex items-center gap-2">
                 <EyeIcon className="w-4 h-4" />
-                100 view
+                {url.views} view
             </span>
           </div>
         </li>
-      </ul>
-    </div>
+       ))} 
+    </ul>
   );
 }
--- components/urlContainer.tsx
+++ components/urlContainer.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ShortenForm from './shorten-form'
-import UrlList from './url-list'
+// import { UrlList } from './url-list'
 
 export default function urlContainer() {
   return (
     <div>
         <ShortenForm />
-        <UrlList />
+        {/* <UrlList /> */}
     </div>
   )
 }
